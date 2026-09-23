4b3da524ecf099e8f6d0d616fcb0800762f21ff2
Sitaram Battula <sitaram.batulla@sita.aero>
2026-09-23 17:37:31 +0530

    initial commit

---
+++ src/app/pages/billing/billing.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-billing',
+  styleUrl: './billing.scss',
+  templateUrl: './billing.html',
+})
+export class Billing {}
